222b4f8fee2144f66e84c7d368b33558699b71c6
DeeTheDev <deethedev@gmail.com>
2020-08-31 19:59:20 -0400

    Removed unnecessary variable / used destructuring instead

--- routes/index.js
+++ routes/index.js
@@ -1,87 +1,88 @@
 let express = require("express");
 let router = express.Router({ mergeParams: true });
 let passport = require("passport"),
-  User = require("../models/user");
+    User = require("../models/user");
 
 // AUTH ROUTES
 router.get("/login", function (req, res) {
-  res.render("login");
+    res.render("login");
 });
 
 router.post(
-  "/login",
-  passport.authenticate("local", {
-    successRedirect: "/",
-    failureRedirect: "/user/login",
-    failureFlash: true,
-  }),
-  function (req, res) {}
+    "/login",
+    passport.authenticate("local", {
+        successRedirect: "/",
+        failureRedirect: "/user/login",
+        failureFlash: true,
+    }),
+    function (req, res) {}
 );
 
 //Show register form
 router.get("/register", function (req, res) {
-  res.render("register");
+    res.render("register");
 });
 
 //Create new user
 router.post("/register", function (req, res) {
-  var usernameStr = req.body.username;
-  var username = usernameStr;
-  var { profileImg, password, password2 } = req.body;
-  let errors = [];
+    var { username, profileImg, password, password2 } = req.body;
+    let errors = [];
 
-  //check required fields
-  if (!username || !password || !password2) {
-    errors.push({ msg: "Fill in all fields that apply" });
-  }
-  //check passwords match
-  if (password !== password2) {
-    errors.push({ msg: "Passwords do not match" });
-  }
-  // Check pass length
-  if (password.length < 6) {
-    errors.push({ msg: "Password should be at least 6 characters" });
-  }
-  if (errors.length > 0) {
-    res.render("register", { errors, username, password, password2 });
-  } else {
-    //validate passed
-    User.findOne({ username }, function (err, user) {
-      if (user) {
-        //User exists
-        errors.push({ msg: "Username is already registered" });
-        res.render("register", {
-          errors,
-          username,
-          password,
-          password2,
+    //check required fields
+    if (!username || !password || !password2) {
+        errors.push({ msg: "Fill in all fields that apply" });
+    }
+    //check passwords match
+    if (password !== password2) {
+        errors.push({ msg: "Passwords do not match" });
+    }
+    // Check pass length
+    if (password.length < 6) {
+        errors.push({ msg: "Password should be at least 6 characters" });
+    }
+    if (errors.length > 0) {
+        res.render("register", { errors, username, password, password2 });
+    } else {
+        //validate passed
+        User.findOne({ username }, function (err, user) {
+            if (user) {
+                //User exists
+                errors.push({ msg: "Username is already registered" });
+                res.render("register", {
+                    errors,
+                    username,
+                    password,
+                    password2,
+                });
+            } else {
+                var userInfo = {
+                    username: username,
+                    profileImg: profileImg,
+                };
+                User.register(new User(userInfo), req.body.password, function (
+                    err,
+                    user
+                ) {
+                    if (err) {
+                        console.log(err);
+                    } else {
+                        req.flash(
+                            "success_msg",
+                            "You are now registered and can log in"
+                        );
+                        res.redirect("login");
+                    }
+                });
+            }
         });
-      } else {
-        var userInfo = {
-          username: username,
-          profileImg: profileImg,
-        };
-        User.register(new User(userInfo), req.body.password, function (
-          err,
-          user
-        ) {
-          if (err) {
-            console.log(err);
-          } else {
-            req.flash("success_msg", "You are now registered and can log in");
-            res.redirect("login");
-          }
-        });
-      }
-    });
-  }
+    }
 });
 
 //Logout Handler
 router.get("/logout", function (req, res) {
-  req.logout();
-  req.flash("success_msg", "You are logged out");
-  res.redirect("/user/login");
+    req.logout();
+    req.flash("success_msg", "You are logged out");
+    res.redirect("/user/login");
 });
 
 //Global Router
