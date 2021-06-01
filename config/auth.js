b3ecb3c98372dabe8382388c3c6cc6eba6935df0
Denilson V <deethedev@gmail.com>
2021-06-01 09:02:55 -0400

    Updated naming convention

--- config/auth.js
+++ config/auth.js
@@ -1,4 +1,5 @@
-var Project = require("../models/question");
+let Question = require("../models/question");
+let Comment = require("../models/comment");
 middlewareObj = {};
 
 middlewareObj.userIsLogged = (req, res, next) => {
@@ -12,14 +13,14 @@ middlewareObj.userIsLogged = (req, res, next) => {
 
 middlewareObj.checkIfOwner = (req, res, next) => {
     if (req.isAuthenticated()) {
-        //find the Project with ID
-        Project.findById(req.params.id || req.params.proj_id)
+        //find the Question with ID
+        Question.findById(req.params.id || req.params.proj_id)
             .populate("author", "_id")
-            .exec((err, foundProject) => {
+            .exec((err, foundQuestion) => {
                 if (err) {
                     res.render("errors/project", { projectID: req.params.id });
                 } else {
-                    if (foundProject.author._id.equals(req.user._id)) {
+                    if (foundQuestion.author._id.equals(req.user._id)) {
                         next();
                     } else {
                         req.flash(
@@ -30,6 +31,36 @@ middlewareObj.checkIfOwner = (req, res, next) => {
                     }
                 }
             });
+    } else {
+        req.flash("info_msg", "You must be logged in to access this page.");
+        res.redirect("back");
+    }
+};
+middlewareObj.checkIfCommentOwner = (req, res, next)  => {
+    if (req.isAuthenticated()) {
+        //find the Question with ID
+        Comment.findById(req.params.comment_id)
+            .populate("author", "_id")
+            .exec((err, comment) => {
+                if(err){      
+                    req.flash(
+                        "info_msg",
+                        "There was an error finding the comment."
+                        );
+                    res.redirect("back");
+                }else {
+                    if(comment.author._id.equals(req.user._id)){
+                        next();
+                    }else {
+                        req.flash(
+                            "info_msg",
+                            "You are not the authorized comment owner."
+                            );
+                        res.redirect("back");
+                    };
+                }
+            });
+
     } else {
         req.flash("info_msg", "You must be logged in to access this page.");
         res.redirect("back");
