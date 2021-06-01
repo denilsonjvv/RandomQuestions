e18a7d1019913e36d42e134f9cc03ccd52f0aabf
Denilson V <deethedev@gmail.com>
2021-06-01 09:05:09 -0400

    Referenced comments in UpdatesSchema

--- models/updates.js
+++ models/updates.js
@@ -10,12 +10,21 @@ var updatesSchema = new mongoose.Schema({
         type: mongoose.Schema.Types.ObjectId,
         ref: "Question", //Data from User model schema
     },
+    comment: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Comment", //Data from User model schema
+    },
     action: String,
     lastUpdated: {
         type: Date,
         format: "%Y-%m-%d%",
         default: new Date(),
     },
+    date: {
+        type: Date,
+        default: Date.now,
+        format: "%Y-%m-%d%",
+    }
 });
 
 module.exports = mongoose.model("Updates", updatesSchema); // Needed evertime we require mongoose
