40e37a5e86c0b3d4389e469345466c2f18d474ac
Denilson Velasquez <deethedev@gmail.com>
2021-11-06 11:11:03 -0400

    Update seeds file for populating database

--- seeds.js
+++ seeds.js
@@ -1,84 +1,78 @@
 const mongoose = require("mongoose");
-const Questions = require("./models/question");
+const Question = require("./models/question");
 const Comment = require("./models/comment");
 const Updates = require("./models/updates");
+const Topic = require("./models/topic");
+const { ObjectID } = require("bson");
 
-const { Types: { ObjectId } } = mongoose;
-
-let data = [
-    // {
-    //     title: "How big is the moon?",
-    //     description: "the Moon could be very very large",
-    //     author: new mongoose.mongo.ObjectId("5f567c4a4b2b062210af2f3f"),
-    // },
-    // {
-    //     title: "Is maisel cute af or what?",
-    //     description: "my itty itty is the bestest",
-    //     author: new mongoose.mongo.ObjectId("5f567c4a4b2b062210af2f3f"),
-    // },
-    {
-        title: "Am I a hungry hippo?",
-        description: "Yes very much, im hungry rn!",
-        author: {
-            '_id': mongoose.Types.ObjectId("5f567c4a4b2b062210af2f3f"),
-            'username': 'DeeTheDev',
-            'profileImg': 'i-3.png'
-        }
-    }
-]
+// This is the object _id of the first user in the database
+const userObj = new ObjectID("618376fa3f01fc1d59f6b239");
+let questionData = {
+    title: "What is the largest thing on earth?",
+    description: "Just a question i wanted to ask okay? :D",
+    author: userObj
+}
 async function seedDB() {
-    await Questions.remove({}, (err) => {
-        if(err) return console.log(err);
+    await Question.remove({}, (err) => {
+        if (err) return console.log(err);
         console.log("removed questions!");
     })
     await Updates.remove({}, (err) => {
-        if(err) return console.log(err);
+        if (err) return console.log(err);
         console.log("removed comments!");
     })
     await Comment.remove({}, (err) => {
-        if(err) return console.log(err);
+        if (err) return console.log(err);
         console.log("removed updates!");
     })
-    data.forEach( async (seed)=> {
-        let question = await Questions.create(seed);
-        if(question) {
-            try{
-                let commentData= [
-                    {
-                        author: {
-                            '_id': mongoose.Types.ObjectId("5f567c4a4b2b062210af2f3f"),
-                            'username': 'DeeTheDev',
-                            'profileImg': 'i-3.png'
-                        },
-                        question: question.id,
-                        text: "Blaaaaaaaaah...",
-                    },
-                ]
-                let newComment =   Comment.create(commentData);
-                
-                 question.comments.push(newComment);
-                 question.save();
-                console.log("Created new Comment!")
-            }catch(err){
-                console.log(err)
-            }
-        }
-
-
-        // await Questions.create(seed, async (err, question) => {
-        //     if(err){
-        //         console.log(err);
-        //     } else {
-        //         console.log("added question!");
-        //         Comment.create(commentData,  (err, comment) => {
-        //                 if (err) return console.log(err);
-        //                 await question.comments.push(comment);
-        //                 question.save();
-        //                 console.log("Created new Comment!")
-        //             }
-        //         )
-        //     }
-        // })
+    await Topic.remove({}, (err) => {
+        if (err) return console.log(err);
+        console.log("removed topics!");
     })
+    try {
+        const newQuestion = await Question.create(questionData);
+        const updatesData = {
+            author: questionData.author,
+            question: newQuestion,
+            action: "created a question",
+        };
+        const newUpdate = await Updates.create(updatesData);
+        const commentData = {
+            author: userObj,
+            question: newQuestion,
+            text: "Awesome questtionnnnn!"
+        }
+        const newComment = await Comment.create(commentData);
+        const topicsData = [
+            {
+                title: "developers",
+                questions: newQuestion
+            },
+            {
+                title: "all",
+                questions: newQuestion
+            },
+            {
+                title: "these",
+                questions: newQuestion
+            },
+            {
+                title: "topics",
+                questions: newQuestion
+            }
+        ]
+        const newTopic = await Topic.insertMany(topicsData);
+        newTopic.forEach((topic) => {
+            newQuestion.topics.push(topic);
+        })
+        newQuestion.comments.push(newComment);
+        newQuestion.updates.push(newUpdate);
+        await newComment.save();
+        await newUpdate.save();
+        await newQuestion.save();
+        console.log("Saved new updates and question, check it out below here");
+    } catch (err) {
+        console.log("there was an error:", err)
+    }
 }
 module.exports = seedDB;
