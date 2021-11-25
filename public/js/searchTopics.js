62477b7ce26e984af5a714c966de6ef88f603780
Denilson Velasquez <deethedev@gmail.com>
2021-11-24 20:21:26 -0500

    update

--- public/js/searchTopics.js
+++ public/js/searchTopics.js
@@ -1,28 +1,46 @@
 //Search members to project function
 let resultList = [];
-function addMember() {
-  let addTopic = document.getElementById("addTopic");
+
+function addTopic() {
+  let topicContainer = document.getElementById("topicContainer");
   resultList.forEach((item) => {
     item.addEventListener("click", () => {
       const topicText = item.textContent;
-      const newInput = `<label class='checkbox-label'><input type='checkbox' name='topics' value='${topicText}' checked></input><span class='checkbox-custom'></span>${topicText}</label> `;
-      addTopic.innerHTML += newInput;
-    })
-  })
+      const newInput = `<label class='chosenTopic checkbox-label'><input type='checkbox' name='topics' value='${topicText}' checked>${topicText} <span onClick="this.parentElement.remove();"><i class="fas fa-times"></i></span></label>`;
+      topicContainer.innerHTML += newInput;
+      item.remove();
+    });
+  });
 }
 function renderResults(search_value, topicsData) {
   const topicsList = $("#topicsList");
+  // const chosenTopics = document.querySelectorAll(".chosenTopic");
   topicsList.html("");
-  if (topicsData.length == 0) return topicsList.append(`<span>No topics found with ${search_value} </span>`);
+  if (topicsData.length == 0) return topicsList.append(`<span>No topics found with ${search_value}</span>`);
   topicsData.forEach(topic => {
     const topicElement = document.createElement("li");
     topicElement.innerHTML = topic.title;
+
+    // TODO: Need to check if chosen topics match the topic title 
+      // - If so, it shouldn't be created, 
+      // - else create element and append to list
+
+    // chosenTopics.forEach((chosenTopic) => {
+    //   if (topic.title.trim() == chosenTopic.textContent.trim()) {
+    //     console.log(`${topic.title} is chosen.`)
+    //     return
+    //   } 
+    //   resultList.push(topicElement);
+    //   topicsList.append(topicElement);
+    // })
     resultList.push(topicElement);
     topicsList.append(topicElement);
+    
   });
-  addMember();
+  addTopic();
 }
-let searchTopics = debounce((arg) => {
+
+const searchTopics = debounce((arg) => {
   resultList = [];
   const searchValue = arg.trim();
   const topicsPopup = $("#topicsPopup");
@@ -43,7 +61,7 @@ let searchTopics = debounce((arg) => {
     renderResults(searchValue, topicsData);
     searchInput.removeClass("loading");
   })
-  .fail(function (err) {
+  .fail((err) => {
     console.log(err);
   });
 }, 500); // debounce data every 500ms from each 2 keystrokes
@@ -53,7 +71,7 @@ function debounce(func, wait, immediate) {
   return function () {
     let context = this;
     args = arguments;
-    let later = function () {
+    let later = () => {
       timeout = null;
       if (!immediate) func.apply(context, args);
     };
