638d92532a5414b2ce770b76a56436e4f9a20c76
Denilson Velasquez <deethedev@gmail.com>
2021-12-01 13:10:03 -0500

    Updated controller postLogin and refactored post register function

--- controllers/index.js
+++ controllers/index.js
@@ -4,60 +4,56 @@ const User = require("../models/user");
 module.exports = {
     postLogin(req, res, next) {
         passport.authenticate("local", {
-            successRedirect: "/",
+            successRedirect: req.session.returnToURL || "/",
             failureRedirect: "/user/login",
             failureFlash: true,
-        })(req, res, next);
+        })(req, res, () => {
+            console.log(req.session.returnToURL);
+        });
     },
     logout(req, res) {
+        // Remove last session url path if logout
+        req.session.returnToURL = undefined;
         req.logout();
         req.flash("success_msg", "You are logged out");
-        res.redirect("/user/login");
+        res.redirect("/");
     },
      async postRegister(req, res) {
-        let { username, profileImg, password, password2 } = req.body;
+        const { username, profileImg, password, password2 } = req.body;
         let errors = [];
+        if (!username.trim() || !password || !password2) errors.push({ msg: "Fill in all fields that apply" });
+        if (password !== password2) errors.push({ msg: "Passwords do not match" });
+        if (password.length < 6) errors.push({ msg: "Password should be at least 6 characters" });
+        if (errors.length > 0) return res.render("register", { errors, username, password, password2 });
 
-        //check required fields
-        if (!username.trim() || !password || !password2) {
-            errors.push({ msg: "Fill in all fields that apply" });
-        }
-        //check passwords match
-        if (password !== password2) {
-            errors.push({ msg: "Passwords do not match" });
-        }
-        // Check pass length
-        if (password.length < 6) {
-            errors.push({ msg: "Password should be at least 6 characters" });
-        }
-        if (errors.length > 0) {
-            res.render("register", { errors, username, password, password2 });
+        const user = await User.findOne({username});
+        if (user) {
+            errors.push({ msg: "Username is already registered" });
+            res.render("register", {
+                errors,
+                username,
+                password,
+                password2,
+            });
         } else {
-            let user = await User.findOne({username});
-            if (user) {
-                //User exists
-                errors.push({ msg: "Username is already registered" });
+            try{
+                const userData = {
+                    username,
+                    profileImg,
+                };
+                const registerUser = await User.register(new User(userData), password);
+                req.flash(
+                    "success_msg",
+                    "You are now registered and can log in"
+                );
+                res.redirect("login");
+            }catch(err){
+                errors.push({ msg: "An unexpected error occured while registering your account, try again." });
                 res.render("register", {
                     errors,
                     username,
                     password,
                     password2,
-                });
-            } else {
-                var userInfo = {
-                    username,
-                    profileImg,
-                };
-                User.register(new User(userInfo), password, (err) => {
-                    if (err) {
-                        console.log(err);
-                    } else {
-                        req.flash(
-                            "success_msg",
-                            "You are now registered and can log in"
-                        );
-                        res.redirect("login");
-                    }
                 });
             }
         }
